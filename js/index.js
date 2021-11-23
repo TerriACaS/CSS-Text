1542427a9b26a42d7723a42787d7c004c5bf644e
TerriACas <trra53@gmail.com>
2021-11-23 08:03:55 +0800

    UPDATE

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
+window.onload = function () {
+  alert("This site is desgned for mobile browsers. It is not recommended to use a PC/Desktop.");
+}
+
 function changebg(url) {
+  document.querySelector("#credits").style.display = "none";
+
   let p = document.querySelector("#text");
   p.style.background = `url(\"${url}\")`;
   p.style.backgroundSize = "cover";
@@ -9,23 +15,23 @@ function changebg(url) {
 
 function ontextchange(ev) {
   let el = ev.srcElement;
-  
-  document.querySelector("#text").innerText = el.value;
+
+  document.querySelector("#text").innerHTML = el.value.replace("\n", "");
 }
 
 function onurlchange(ev) {
   let el = ev.srcElement;
 
   let url = el.value;
-  
+
   changebg(url);
 }
 
 function onfilechange(ev) {
   let el = ev.srcElement;
   let file = el.files[0];
-  
+
   let url = URL.createObjectURL(file);
-  
+
   changebg(url);
 }
